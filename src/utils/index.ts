7f839ecd0b5f6b11161907a40f21e2041d29cfff
Zak Nesler <zak@nesler.dev>
2022-08-06 23:21:33 -0400

    Fix export

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@ import { serialize } from 'cookie'
 import dayjs from 'dayjs'
 import type { CookieSerializeOptions } from 'next/dist/server/web/types'
 
-export const isProd = process.env.NODE_ENV === 'production'
+export const isProd = () => process.env.NODE_ENV === 'production'
 
 export const formatCookie = (
   name: string,
