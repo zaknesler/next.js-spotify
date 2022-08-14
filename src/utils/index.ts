4e12ba406a14a0fc876c03c5b8ac3032a820778a
Zak Nesler <zak@nesler.dev>
2022-08-13 22:54:59 -0400

    Add env helper

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,10 @@ import { serialize } from 'cookie'
 import dayjs from 'dayjs'
 import type { CookieSerializeOptions } from 'next/dist/server/web/types'
 
-export const isProd = () => process.env.NODE_ENV === 'production'
+export const isEnv = (env: 'production' | 'development') =>
+  process.env.NODE_ENV === env
+
+export const isProd = () => isEnv('production')
 
 export const formatCookie = (
   name: string,
